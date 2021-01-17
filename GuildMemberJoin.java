304c4609eebaaba7b0c65ee1f526c48c8f205f0f
yifanl27 <70334089+yifanl27@users.noreply.github.com>
2021-01-17 08:02:23 -0500

    Add files via upload

---
+++ GuildMemberJoin.java
@@ -0,0 +1,22 @@
+package main.bot;
+
+import java.util.Random;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class GuildMemberJoin extends ListenerAdapter{
+    String[]messages= { "Welcome [member]! I am Safe Arrival and I will be keeping track of your destinations in order to ensure your safety.",
+            "Hello [member], I am Safe Arrival!", "Hi[member]! I am excited to help you have a fun and safe journey."
+    };
+    public void onGuildMemberJoin(GuildMemberJoinEvent event){
+        Random rand= new Random();
+        int number = rand.nextInt(messages.length);
+        EmbedBuilder join= new EmbedBuilder();
+        join.setColor(0x0D166);
+        join.setDescription(messages[number].replace("[member]", event.getMember().getAsMention())+ " Please go to <#800078659141894184> to choose your role!");
+
+        long chanID = 800231403550670848L;
+        event.getGuild().getTextChannelById(chanID).sendMessage(join.build()).queue();
+    }}
