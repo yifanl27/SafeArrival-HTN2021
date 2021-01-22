cd15f78765573fb2e84a8caadb53a56961722962
Minhat Mustafa <65133492+minhatmustafa@users.noreply.github.com>
2021-01-22 01:48:34 -0500

    Update RoleReactions.java

--- RoleReactions.java
+++ RoleReactions.java
@@ -9,6 +9,7 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class RoleReactions extends ListenerAdapter {
 
+    // The following IDs are specific to a certain Discord server. They must be replaced before implementing the Bot properly.
     final long channelID = 800078659141894184L;
     final long roleID_RA = 799894414184153148L;
     final long roleID_St = 799847084252987442L;
@@ -22,10 +23,12 @@ public class RoleReactions extends ListenerAdapter {
             if (e.getJDA().getRoleById(roleID_RA) == null) {
                 return;
             }
-
+            
+            // The following Java source code is for the school emoji.
             if ((e.getReactionEmote().getName().equals("\uD83C\uDFEB"))) {
                 e.getGuild().addRoleToMember(e.getUserId(), e.getJDA().getRoleById(roleID_RA)).queue();
-
+            
+            // The following Java source code is for the stack of books emoji.
             } else if ((e.getReactionEmote().getName().equals("\uD83D\uDCDA"))) {
                 e.getGuild().addRoleToMember(e.getUserId(), e.getJDA().getRoleById(roleID_St)).queue();
             }
