a3ec0a5c959ada53c79213acb436b95ea0d685db
Minhat Mustafa <65133492+minhatmustafa@users.noreply.github.com>
2021-01-22 01:53:12 -0500

    Update GuildMemberJoin.java

--- GuildMemberJoin.java
+++ GuildMemberJoin.java
@@ -6,6 +6,8 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+// Note: All channel IDs in this class are specific to a certain Discord server. Please replace these IDs before using the Bot.
+
 public class GuildMemberJoin extends ListenerAdapter{
     String[]messages= { "Welcome [member]! I am Safe Arrival and I will be keeping track of your destinations in order to ensure your safety.",
             "Hello [member], I am Safe Arrival!", "Hi[member]! I am excited to help you have a fun and safe journey."
@@ -16,7 +18,7 @@ public class GuildMemberJoin extends ListenerAdapter{
         EmbedBuilder join= new EmbedBuilder();
         join.setColor(0x0D166);
         join.setDescription(messages[number].replace("[member]", event.getMember().getAsMention())+ " Please go to <#800078659141894184> to choose your role!");
-
+        
         long chanID = 800231403550670848L;
         event.getGuild().getTextChannelById(chanID).sendMessage(join.build()).queue();
     }}
